eec3006a84dfd670b6fb4930640ddd10c2b6ebc8
Chris Stone <chris.stone-ext@cygest.fr>
2024-10-20 10:07:32 +0400

    nouveau feature

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,10 +1,10 @@
-namespace GamesStore.Models
+namespace GamesStore.Models;
 
 public class Game
 {
     public int Id {get;set;}
     public required string Name {get;set;}
-    public require required string Genre {get;set;}
+    public  required string Genre {get;set;}
     public decimal Price {get;set;}
     public DateTime ReleaseDate {get;set;}
 }
